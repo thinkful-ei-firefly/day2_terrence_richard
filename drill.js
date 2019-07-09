b268a01779722d303e8739a17cc9b4960924bb6c
Ramarduk <ramarduk@gmail.com>
2019-07-09 10:24:54 -0600

    added min

--- drill.js
+++ drill.js
@@ -17,3 +17,20 @@ function max(arr) {
 
 console.log(max([0,5,7,3,4])); //-> 7
 console.log('something');
+
+function min(arr) {
+  if (!arr) {
+    return null;
+  }
+  let currentMin = arr[0];
+  let counter = 0;
+  while(counter < arr.length) {
+    if (currentMin > arr[counter]) {
+      currentMin = arr[counter]; 
+    }
+    counter += 1;
+  }
+  return currentMin;
+}
+
+console.log(min([9,5,7,3,4])); //-> 3
