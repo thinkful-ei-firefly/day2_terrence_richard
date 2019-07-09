e44f22f62e98c844d1f4f24cb5c24e4e0f4537e1
Ramarduk <ramarduk@gmail.com>
2019-07-09 14:07:42 -0600

    updated foreach.js

---
+++ foreach.js
@@ -0,0 +1,10 @@
+'use strict';
+let turtleMoves=[[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+
+turtleMoves.filter(function(moves) {
+    moves.forEach(function(i) {
+        if( i > 0){
+            return moves;
+    }
+}
+}
